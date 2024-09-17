ce224d430d841d1085a6ce57eb55b7b74f57e168
Ivan <ivan@ascendas.ie>
2024-09-18 00:03:51 +0400

    font-awesome

--- src/features/auth/components/LoginForm.component.tsx
+++ src/features/auth/components/LoginForm.component.tsx
@@ -3,6 +3,8 @@ import { LoginDto } from "../../../shared/dtos/login.dto";
 import { CheckTempPasswordRequestDto } from "../../../shared/dtos/tempPassword.dto";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { SelectLanguage } from "../../../shared/components/SelectLanguage";
+import { useState } from "react";
 
 type LoginFormProps = {
   onSubmit: (dto: CheckTempPasswordRequestDto) => void;
@@ -12,7 +14,7 @@ export const LoginFormComponent = ({
   onSubmit,
 }: LoginFormProps): JSX.Element => {
   const { t } = useTranslation();
-
+  const [showPassword, setShowPassword] = useState(false);
   const {
     register,
     formState: { errors },
@@ -28,6 +30,12 @@ export const LoginFormComponent = ({
   return (
     <div className="auth-container">
       <div className="login-form">
+        <div className="login-form__logo">
+          <img
+            src={require("../../../assets/img/custom/logo.png")}
+            alt="logo"
+          />
+        </div>
         <div className="login-form__title">{t("SIGN_IN.TITLE")}</div>
         <div className="login-form__subtitle">{t("SIGN_IN.SUB_TITLE")}</div>
         <form onSubmit={handleSubmit(submitHandler)}>
@@ -43,27 +51,43 @@ export const LoginFormComponent = ({
               </p>
             )}
           </div>
-          <div className="login-form__input-container account-password">
+          <div className="input-container login-form__input-container account-password">
             <input
               {...register("password", { required: true })}
               aria-invalid={errors.password ? "true" : "false"}
               placeholder={t("LABELS.PASSWORD")}
-              type="password"
+              type={showPassword ? "text" : "password"}
             />
+            <div
+              className="password-icon"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <i
+                className={showPassword ? "fas fa-eye-slash" : "fas fa-eye"}
+              ></i>
+            </div>
+
             {errors.password?.type === "required" && (
               <p role="alert" className="error">
                 {t("ERRORS.REQUIRED")}
               </p>
             )}
           </div>
-          <button type="submit" className="button-primary">
+          <button type="submit" className="button-primary" disabled={false}>
             {t("SIGN_IN.BUTTONS.LOGIN")}
           </button>
           <div className="login-form__link-holder">
             <Link to={"/"}>{t("SIGN_IN.BUTTONS.FORGOT_PASSWORD")}</Link>
           </div>
+          <div className="lang-dropdown">
+            <SelectLanguage />
+          </div>
         </form>
       </div>
+      <footer>
+        {new Date().getFullYear()} {t("APP.FOOTER_TEXT")}{" "}
+        <Link to="/policy"> {t("MAIN.MENU.DATA_POLICY")}</Link>
+      </footer>
     </div>
   );
 };
---
+++ src/shared/components/SelectLanguage.tsx
@@ -0,0 +1,14 @@
+import { useTranslation } from "react-i18next";
+
+export const SelectLanguage = (): JSX.Element => {
+  const { i18n } = useTranslation();
+  return (
+    <select
+      value={i18n.language}
+      onChange={(e) => i18n.changeLanguage(e.target.value)}
+    >
+      <option value="gb">English</option>
+      <option value="ie">Gaeilge</option>
+    </select>
+  );
+};
