6f99ffde8eef7a45f70ea82bf7cc367f2318e28a
ivan <ivan@ascendas.ie>
2024-12-02 13:51:28 +0400

    contact-us page

--- src/features/contacts/Contacts.component.tsx
+++ src/features/contacts/Contacts.component.tsx
@@ -1,9 +1,32 @@
 import React from "react";
-import './Contacts.component.scss';
+import "./Contacts.component.scss";
 import { Box } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../shared/redux/slices/authSlice";
 
 export const ContactsComponent = (): JSX.Element => {
-    return (<Box sx={{ flexGrow: 1 }} >
-        Contacts
-    </Box>);
+  const { t } = useTranslation();
+  const user = useSelector(selectUser);
+  return (
+    <Box sx={{ flexGrow: 1 }} className=" contact-us">
+      <Box className=" personal_box contact-info-block">
+        <Box className="title">
+          <h1>{t("CONTACT_US.TITLE")}</h1>
+        </Box>
+        <Box>
+          {t("CONTACT_US.PHONE")}: <a href="tel:061 556600">061 556600</a>
+        </Box>
+
+        <Box>
+          {t("CONTACT_US.EMAIL")}:{" "}
+          <a
+            href={`mailto:Hapcollections@limerick.ie?subject=HAP ID ${user?.customerNo}`}
+          >
+            Hapcollections@limerick.ie
+          </a>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
